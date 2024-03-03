2771b1fbd884526958b26745300eea3548de02e5
Uladimir Hrydzin <hrydzin@gmail.com>
2024-03-03 22:17:55 +0400

    fix: createProfileInput

--- src/routes/graphql/types/profiletypes.ts
+++ src/routes/graphql/types/profiletypes.ts
@@ -5,36 +5,6 @@ import { PrismaClient } from "@prisma/client";
 import { Profile } from "../intefaces/model.js";
 
 
-// export const ProfileType = new GraphQLObjectType({
-//     name: 'Profile',
-//     fields: {
-//       id: { type: UUIDType },
-//       isMale: { type: GraphQLBoolean },
-//       yearOfBirth: { type: GraphQLInt },
-//       userId: { type: UUIDType },
-//       // ??
-//       // memberTypeId: { type: MemberTypeIdEnum },
-//       memberTypeId: { type: GraphQLString },
-
-//       memberType: {
-//         type: MemberType,
-//         args: {
-//             id: { type: new GraphQLNonNull(MemberTypeIdEnum) }
-//         },
-//         resolve: async (_, args: { id: string }, { prisma }: { prisma: PrismaClient }) => {
-//         const { id } = args;
-//         const memberType = await prisma.memberType.findUnique({
-//             where: {
-//             id
-//             }
-//         });
-
-//         return memberType;
-//         }
-//     },
-//     },
-//   });
-
 export const ProfileType = new GraphQLObjectType({
     name: 'Profile',
     fields: () => ({
@@ -64,7 +34,7 @@ export const CreateProfileInput = new GraphQLInputObjectType({
     fields: () => ({
         isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
         yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
-        memberTypeId: { type: new GraphQLNonNull(GraphQLString) },
-        userId: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+        memberTypeId: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+        userId: { type: new GraphQLNonNull(GraphQLString) },
     }),
 });
